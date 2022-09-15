ee81585158664dac5c6df114b807c5ed462b4d8f
Irina Dolgova <claycharm@yandex.ru>
2022-09-15 08:46:48 +0300

    edit method setup

--- src/test/java/TestDebetCart.java
+++ src/test/java/TestDebetCart.java
@@ -20,7 +20,7 @@ public class TestDebetCart {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
